cd07946893315092fdd7451d5cf7a39b3a3a57a3
Stanislav <XXXmauzer@gmail.com>
2023-10-27 06:18:24 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// - Seminar01 -
+// - Web Technology -
+
+// http - не имеет шифрование, но работает быстрее
+// https - имеет шифрование, но работа замедленна
+
+// Структура страницы : шапка, зона контента, зона виджетов, подвал.
+
+// Сайт работает на протоколе https
+
+// Хедер : <header itemscope="" itemtype="http://schema.org/WPHeader" id="masthead" class="site-header">
+
+// Контент : <div id="content" class="site-content">
+
+// Футер : <footer itemscope="itemscope" itemtype="http://schema.org/WPFooter" id="colophon" class="site-footer" role="contentinfo">
+
